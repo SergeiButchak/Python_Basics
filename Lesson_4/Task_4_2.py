d40a154016a7fce75a5cbd9e9ccde90db1309b99
Sergei.Butchak <Sergei.Butchak@gmail.com>
2020-11-12 18:55:24 +0400

    Change solution

--- Lesson_4/Task_4_2.py
+++ Lesson_4/Task_4_2.py
@@ -1,9 +1,6 @@
 import random
 
 arr = list([random.randint(0, 100) for el in range(30)])
-new_arr = []
-for ind in range(1, len(arr)):
-    if arr[ind] > arr[ind - 1]:
-        new_arr.append(arr[ind])
+new_arr = list([arr[ind] for ind in range(1, len(arr)) if arr[ind] > arr[ind - 1]])
 print(f"Исходный массив: {arr}")
 print(f"Новый массив: {new_arr}")
