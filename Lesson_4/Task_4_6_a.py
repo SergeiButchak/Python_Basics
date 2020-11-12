45dac70f757288868ed1d5bd631582f23d652151
Sergei.Butchak <Sergei.Butchak@gmail.com>
2020-11-12 19:17:33 +0400

    Task_4_6_a complete

---
+++ Lesson_4/Task_4_6_a.py
@@ -0,0 +1,17 @@
+from itertools import count
+from sys import argv
+
+if len(argv) == 3:
+    try:
+        a = int(argv[1])
+        b = int(argv[2])
+        for el in count(a):
+            if el > b:
+                break
+            print(el)
+
+    except Exception as exc:
+        print(exc)
+else:
+    print("Для запуска генератор требуется запустить скрипт с двумя аргументами <начальное значение> <максимальное значение>")
+    
